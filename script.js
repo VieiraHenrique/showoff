029231601d965620aa742311cb22ee85e52c0432
Henrique Andrade Vieira <me.henriquevieira@gmail.com>
2021-05-24 18:17:11 +0200

    final version

--- script.js
+++ script.js
@@ -29,3 +29,22 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+/* LAXXX */
+
+lax.init();
+
+// Add a driver that we use to control our animations
+lax.addDriver("scrollY", function () {
+  return window.scrollY;
+});
+
+// Add animation bindings to elements
+lax.addElements(".selector", {
+  scrollY: {
+    translateX: [
+      ["elInY", "elCenterY", "elOutY"],
+      [0, "screenWidth/2", "screenWidth"],
+    ],
+  },
+});
